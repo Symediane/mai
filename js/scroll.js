d8c67585688018f87268b4d9d04c83883210bd74
damien-simon <damien@symediane.com>
2024-02-16 17:28:09 +0100

    fixed header for vertical scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const $header = document.querySelector(".header");
+  const verticalScroll = document.querySelector(".vertical-scroll .main-content");
 
   let lastScrollTop = 0;
   window.addEventListener("scroll",() => {
@@ -14,4 +15,20 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     false
   );
+
+  if(verticalScroll){
+    verticalScroll.addEventListener("scroll",() => {
+      let st = verticalScroll.scrollTop;
+      if (st <= 10) {
+        $header.classList.remove("--white");
+      }
+      else {
+        $header.classList.add("--white");
+      }
+      lastScrollTop = st <= 10 ? 10 : st;
+    },
+    false
+  );
+  }
+
 });
