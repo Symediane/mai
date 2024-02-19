bca38b89bf5cccf7c1815c824d26be3a4619715f
damien-simon <damien@symediane.com>
2024-02-19 13:55:33 +0100

    add image mobile fan

--- components/home/fan.php
+++ components/home/fan.php
@@ -1,11 +1,19 @@
 <?php
 $image = get_sub_field('image');
+$image_mobile = get_sub_field('image_mobile');
 $texte = get_sub_field('texte');
 
 if($image) {
 ?>
 <section class="section fan" data-block-section>
-  <?= getPictureMedia($image['id'], 'full', ['mask' => false]); ?>
+  <div class="image<?= $image_mobile ? ' image__desktop' : '' ?>">
+    <?= getPictureMedia($image['id'], 'full', ['mask' => false]); ?>
+  </div>
+  <?php if ($image_mobile) { ?>
+    <div class="image__mobile">
+      <?= getPictureMedia($image_mobile['id'], 'full', ['mask' => false]); ?>
+    </div>
+  <?php } ?>
   <?php if ($texte) { ?>
   <div class="fan__texte">
     <?= $texte; ?>
